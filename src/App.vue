96499ebe11cc93ae981c74248c251085f6e30549
costel-dev <costelcuconoiu@gmail.com>
2020-11-10 20:34:57 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>My First VUE Task</h1>
+    <div v-for="(numberInput, index) in numberInputs" v-bind:key="index">
+      <NumberInput v-bind:numberInput="numberInput" />
+    </div>
+    <button v-on:click="addInput">{{ sumOfInputs }}</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NumberInput from "./components/NumberInput.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NumberInput,
+  },
+  data() {
+    return {
+      numberInputs: [10, 20, 25, 25, 20],
+    };
+  },
+  methods: {
+    addInput() {
+      const random = Math.floor(Math.random() * 11);
+      return this.numberInputs.push(random);
+    },
+  },
+  computed: {
+    sumOfInputs() {
+      return this.numberInputs.reduce((total, num) => total + num);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+h1 {
+  margin-bottom: 30px;
+}
+input,
+button {
+  margin: 5px;
+  padding: 10px;
+  text-align: center;
+  font-size: 16px;
+  width: 300px;
 }
 </style>
---
+++ src/components/NumberInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <div>
+      <input type="number" v-bind:value="numberInput" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NumberInput",
+  props: ["numberInput"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
